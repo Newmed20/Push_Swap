640e33e5ff7bace86be6a9240229b640f5936a2f
Newmed20 <hamoda.style.000@gmail.com>
2024-07-07 07:24:26 +0100

    atoi

---
+++ push_swap.c
@@ -0,0 +1,89 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mjadid <mjadid@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/28 11:34:49 by mjadid            #+#    #+#             */
+/*   Updated: 2024/07/07 07:23:56 by mjadid           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+#include "push_swap.h"
+
+int	ft_atoi_check(const char *str, int *xx, int i, long long result)
+{
+	int	sign;
+
+	sign = 1;
+	while (str[i] == ' ' || (str[i] >= 9 && str[i] <= 13))
+		i++;
+	if (str[i] == '-')
+	{
+		sign = -1;
+		i++;
+	}
+	else if (str[i] == '+')
+		i++;
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		result = result * 10 + str[i] - '0';
+		i++;
+	}
+	if (result * sign > 2147483647 || result * sign < -2147483648)
+		*xx = 0;
+	return (result * sign);
+}
+
+int	ft_atoi(const char *str)
+{
+	int	i;
+	int	sign;
+	int	result;
+
+	i = 0;
+	sign = 1;
+	result = 0;
+	while (str[i] == ' ' || (str[i] >= 9 && str[i] <= 13))
+		i++;
+	if (str[i] == '-')
+	{
+		sign = -1;
+		i++;
+	}
+	else if (str[i] == '+')
+		i++;
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		result = result * 10 + str[i] - '0';
+		i++;
+	}
+	return (result * sign);
+}
+
+void	ft_putstr_fd(char *str, int fd)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		write(fd, &str[i], 1);
+		i++;
+	}
+}
+
+
+
+
+int main(int argc , char *argv)
+{
+    int
+    if(argc > 1)
+    {
+        while(argv[i])
+        ft_strjoin()
+    }
+}
